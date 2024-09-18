0255008b17d60266cfe0b2378dfed738bff0e9cf
Steven Huang <sarsky228@gmail.com>
2024-09-18 23:41:43 +0800

    feat: create lists route

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import Main from './ui/Main';
-import Header from './ui/Header';
-import Input from './ui/Input';
-import List from './ui/List';
-import Status from './ui/status';
-import { useTasks } from './context/TasksContext';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import AppLayout from './ui/AppLayout';
+import Lists from './pages/Lists';
 
 function App() {
-  const { tasks } = useTasks();
-  const taskLength = tasks?.length;
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route index element={<List />} />
+          <Route index element={<Navigate replace to='/lists' />} />
+          <Route path='/lists' element={<Lists />} />
         </Route>
       </Routes>
-      {/* <Main>
-        <Header />
-        <Input />
-        <List />
-        {taskLength ? <Status /> : ''}
-      </Main> */}
     </BrowserRouter>
   );
 }
---
+++ src/pages/Lists.jsx
@@ -0,0 +1,19 @@
+import { useTasks } from '../context/TasksContext';
+import Input from '../ui/Input';
+import List from '../ui/List';
+import Status from '../ui/status';
+
+function Lists() {
+  const { tasks } = useTasks();
+  const taskLength = tasks?.length;
+
+  return (
+    <>
+      <Input />
+      <List />
+      {taskLength ? <Status /> : ''}
+    </>
+  );
+}
+
+export default Lists;

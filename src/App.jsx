008c042c9ae5b7e8f37d88c1335dd3c9b8c17951
Steven Huang <sarsky228@gmail.com>
2024-09-19 09:32:28 +0800

    refactor: move all context provider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { DarkModeProvider } from './context/DarkModeContext.jsx';
+import { TasksProvider } from './context/TasksContext.jsx';
+import { FilterProvider } from './context/FilterContext.jsx';
 import AppLayout from './ui/AppLayout';
 import Lists from './pages/Lists';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to='/lists' />} />
-          <Route path='/lists' element={<Lists />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <DarkModeProvider>
+      <FilterProvider>
+        <TasksProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route index element={<Navigate replace to='/lists' />} />
+                <Route path='/lists' element={<Lists />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </TasksProvider>
+      </FilterProvider>
+    </DarkModeProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
-import { DarkModeProvider } from './context/DarkModeContext.jsx';
-import { TasksProvider } from './context/TasksContext.jsx';
-import { FilterProvider } from './context/FilterContext.jsx';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
   <StrictMode>
-    <DarkModeProvider>
-      <FilterProvider>
-        <TasksProvider>
-          <App />
-        </TasksProvider>
-      </FilterProvider>
-    </DarkModeProvider>
+    <App />
   </StrictMode>
 );
